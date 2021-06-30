5cc653ed30745598a2d9e2dd4fa582a9f8a64fca
Md Rakib Uddin <62870441+rakib83346@users.noreply.github.com>
2021-06-30 20:22:54 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+particlesJS("particles-js", {
+    particles: {
+      number: { value: 58, density: { enable: true, value_area: 800 } },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 0.1183721462448409,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 0,
+          opacity_min: 0.016241544246026904,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.7616207289111233,
+        width: 0.4810236182596568
+      },
+      move: {
+        enable: true,
+        speed: 3.206824121731046,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "bubble" },
+        onclick: { enable: true, mode: "repulse" },
+        resize: true
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: {
+          distance: 400,
+          size: 30,
+          duration: 2,
+          opacity: 0.4872463273808071,
+          speed: 3
+        },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
+ 
+  
